a9e7af83d75f6a61f8917d9e707e513141a02206
Galiano N. Quive <84616944+GHOSTGAMERWX@users.noreply.github.com>
2026-01-13 00:25:42 +0200

    Update images in HomeView component
    random texts

--- views/HomeView.tsx
+++ views/HomeView.tsx
@@ -70,7 +70,7 @@ const HomeView: React.FC<HomeViewProps> = ({ onNavigate }) => {
             title="Inspira-te com cores"
             description="Mergulhe em um universo variado de cores e pigmentos. Temos tudo o que você precisa para dar vida à sua visão."
             icon={<Sparkles size={20} />}
-            image="https://images.unsplash.com/photo-1513519245088-0e12902e5a38?auto=format&fit=crop&q=80&w=400"
+            image="https://images.unsplash.com/photo-1495474472287-4d71bcdd2085?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D?auto=format&fit=crop&q=80&w=400"
             onClick={() => onNavigate('gallery')}
             reverse
           />
@@ -81,7 +81,7 @@ const HomeView: React.FC<HomeViewProps> = ({ onNavigate }) => {
             title="Sabor em cada traço"
             description="Faça a tua ordem a partir do nosso menu artesanal. Um bom café é o melhor combustível para a criatividade."
             icon={<Coffee size={20} />}
-            image="https://images.unsplash.com/photo-1541167760496-162955ed8a9f?auto=format&fit=crop&q=80&w=400"
+            image="https://images.unsplash.com/photo-1495474472287-4d71bcdd2085?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D?auto=format&fit=crop&q=80&w=400"
             onClick={() => onNavigate('menu')}
           />
 
